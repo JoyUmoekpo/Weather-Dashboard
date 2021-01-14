74521eb5cd339d1069a1d01b069a7e7c8318983c
Joy <joyumoekpo1@gmail.com>
2021-01-13 21:47:12 -0600

    Final touches

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-var APIKey = "5fd21d5d5826aa9898961eecb3cf4683";
-var citySearch = $("#searchInput").val();
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIKey;
+$(document).ready(function () {
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
+    function currentWeatherNow(city) {
+
+        $("#currentWeather").empty();
+
+        var APIKey = "5fd21d5d5826aa9898961eecb3cf4683";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            var cityName = $("<h2>").html("City: " + response.name);
+            var temperature = ((response.main.temp - 273.15) * 1.80 + 32).toFixed(2) + "°F";
+            var weatherPic = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].weatherPic + "@2x.png");
+            weatherPic.attr("width", 50);
+            var humidity = $("<h4>").html("Humidity: " + response.main.humidity);
+            var windSpeed = $("<h4>").html("Wind Speed: " + response.wind.speed);
+            $("#currentWeather").append(cityName, weatherPic, temperature, humidity, windSpeed);
+        });
+    };
+    
+
+        $("#showWeather").on("click", function (event) {
+            event.preventDefault();
+            var inputCity = $(".form-control").val().trim();
+            // console.log(inputCity);
+            var button = $("<button>");
+            button.addClass("searchHistory");
+            button.text(inputCity);
+            $("#searchResults").prepend(button);
+            currentWeatherNow(inputCity);
+            $(".form-control").val("");
+        });
+    
 });
